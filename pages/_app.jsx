539dd848e2cb0ba921559390e3b3a9a75fcf4af1
gegyhamdani <gegyhamdani@gmail.com>
2020-08-26 17:39:30 +0700

    feat(config): create global config for swr

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { SWRConfig } from 'swr';
+import api from '../src/services/api';
 
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig
+      value={{
+        fetcher: url => api.get(url).then(res => res.data)
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 };
 
 MyApp.propTypes = {
