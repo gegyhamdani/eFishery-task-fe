3be4eb66462d0a205adcecb43e137340180bb853
gegyhamdani <gegyhamdani@gmail.com>
2020-08-26 19:20:10 +0700

    feat(component): create pages component for base component

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Pages from '../src/templates/Pages';
+
+const Home = () => {
+  return (
+    <Pages>
+      <h1>eFishery Task</h1>
+    </Pages>
+  );
+};
+
+export default Home;
