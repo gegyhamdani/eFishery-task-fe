fb75c5a4b227f9721ec70cb4f184f87ad38891c2
gegyhamdani <gegyhamdani@gmail.com>
2020-08-27 15:57:36 +0700

    refactor(style): Add icon exit and redesign width of modal base

--- src/components/molecules/Modal/ModalBase/ModalBase.jsx
+++ src/components/molecules/Modal/ModalBase/ModalBase.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+
 import styles from './index.module.scss';
 
 const ModalBase = ({ isActive, onClose, classNameContent, children }) => {
@@ -28,6 +31,11 @@ const ModalBase = ({ isActive, onClose, classNameContent, children }) => {
           role="button"
           tabIndex="0"
         >
+          <div className={`${styles['modal-base__close-button']}`}>
+            <IconButton aria-label="tambah data" onClick={onClose}>
+              <CloseIcon fontSize="large" />
+            </IconButton>
+          </div>
           {children}
         </div>
       </div>
